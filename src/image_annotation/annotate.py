efba9801a3f8984f53638e60f5ee81e58a601df3
Christian <nielsenchristianole@gmail.com>
2024-03-20 13:20:13 +0100

    improved annotation speed and allowed for more generel data structure in dataset

--- src/image_annotation/annotate.py
+++ src/image_annotation/annotate.py
@@ -1,74 +1,84 @@
 import os
+from os import PathLike
 from PIL import Image
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import tkinter as tk
+from functools import cached_property
+from glob import glob
+from collections import deque
 
 from src.utils.config import Config
 
 
 
 class DatasetAnnotation:
-    def __init__(self, data_folder, output_csv):
+    def __init__(self, data_folder: PathLike, output_csv: PathLike, *, key_to_label_map: dict[str, str] | None=None, ignore_patters: list[str] | None=None):
+        
         self.data_folder = data_folder
         self.output_csv = output_csv
+        self.columns = ['image', 'path', 'label']
+        self.ignore_patters = ignore_patters or ['**/never/**']
+
+        self.key_to_label_map = key_to_label_map or {i: i for i in ['1', '2', '3', '4']}
+
+        self.keys = set(self.key_to_label_map.keys())
+        self.key_err_msg = f"Invalid key pressed, please press {str(sorted(self.keys))[1:-1]}"
+        
         # Make csv file if it does not exist
         if not os.path.exists(self.output_csv):
             # make dir
             os.makedirs(os.path.dirname(self.output_csv), exist_ok=True)
-            df = pd.DataFrame(columns=['image', 'path', 'label'])
+            df = pd.DataFrame(columns=self.columns)
             df.to_csv(self.output_csv, index=False)
-    
-    def append_annotation_to_csv(self, image_id, label):
+        
+        df = pd.read_csv(self.output_csv)
+        self.annotated_set = set(df['path'])
+        self.not_annotated_queue = deque(np.random.permutation(list(set(self.all_paths) - self.annotated_set)))
+        
+    def split_path(self, path: PathLike) -> tuple[str, str, str]:
+        _dir, filename = os.path.split(path)
+        image_id, ext = os.path.splitext(filename)
+        return _dir, image_id, ext
+
+    def append_annotation_to_csv(self, path: PathLike, label: str):
         """Append the annotation to the CSV file."""
         # Append the annotation to the CSV file'
-        path = self.data_folder
-        df = pd.DataFrame([[image_id, path, label]], columns=['image', 'path', 'label'])
+        _, image_id, _ = self.split_path(path)
+        row = [image_id, path, label]
+        df = pd.DataFrame([row], columns=self.columns)
         df.to_csv(self.output_csv, mode='a', index=False, header=not os.path.exists(self.output_csv))
+        self.annotated_set.add(path)
+        self.not_annotated_queue.popleft()
 
-    @property
-    def all_ids(self):
-        """Return all the image ids in the data folder."""
-        return os.listdir(self.data_folder)
+    @cached_property
+    def all_paths(self) -> set[str]:
+        """Return all the image paths in the data folder."""
+        _all = set(glob('**/*.jpg', root_dir=self.data_folder, recursive=True))
+        _all |= set(glob('**/*.png', root_dir=self.data_folder, recursive=True))
+        for pattern in self.ignore_patters:
+            _all -= set(glob(pattern, root_dir=self.data_folder, recursive=True))
+        return _all
     
-    @property
-    def annotated_ids(self):
-        '''Return the image ids that have been annotated.'''
-        if not os.path.exists(self.output_csv):
-            return []
-        else:
-            return pd.read_csv(self.output_csv)['image'].tolist()
+    def get_unannotated_image(self) -> PathLike:
+        """The queue is randomly shuffled, so the first element is a random unannotated image."""
+        return self.not_annotated_queue[0]
 
-    @property
-    def image_ids_not_annotated(self):
-        """Return the image ids that have not been annotated yet."""
-        # Check which images have not been annotated yet
-        if not os.path.exists(self.output_csv):
-            return self.image_ids
-        else:
-            annotated_ids = pd.read_csv(self.output_csv)['image'].tolist()
-            return [image_id for image_id in self.all_ids if image_id not in annotated_ids]
-        
-    def get_random_image(self):
-        images_not_annotated = self.image_ids_not_annotated
-        # get random idx 
-        idx = np.random.randint(0, len(images_not_annotated))
-        return self.image_ids_not_annotated[idx]
-
-    def _display_image(self, image_id):
+    def _display_image(self, path: PathLike):
         """Display the image with the given image_id on self.annotate_ax and self.annotate_fig."""
+        path = os.path.join(self.data_folder, path)
         ax = self.annotate_ax
         fig = self.annotate_fig
-        ax.clear()  
-        image_path = os.path.join(self.data_folder, image_id)
-        img = Image.open(image_path)
+        ax.clear()
+        _, image_id, _ = self.split_path(path)
+        img = Image.open(path)
         ax.imshow(img)  
         # Show image name 
         ax.set_title(image_id)
-            
-        num_annotations = len(self.annotated_ids)
-        n_images = len(self.all_ids)
+        
+        num_annotations = len(self.annotated_set)
+        n_images = len(self.all_paths)
         # Clear the figure to remove previous annotation count text
         fig.texts.clear()
         # Show number of annotated images at the top of the figure
@@ -80,16 +90,17 @@ class DatasetAnnotation:
     def _on_key(self, event):
         """Function to be called when a key is pressed."""
         # Check if the key pressed is a valid annotation
-        if event.key in ['1', '2', '3', '4']:
+        if event.key in self.keys:
             # Append the annotation to the CSV file
-            self.append_annotation_to_csv(self.image_id, event.key)
+            label = self.key_to_label_map[event.key]
+            self.append_annotation_to_csv(self.tmp_image_path, label)
 
             # Check if there are more images to annotate
-            if self.image_ids_not_annotated: 
+            if self.not_annotated_queue: 
                 # Pick a new image to annotate
-                self.image_id = self.get_random_image() 
+                self.tmp_image_path = self.get_unannotated_image() 
                 # Display the new image
-                self._display_image(self.image_id) 
+                self._display_image(self.tmp_image_path) 
             else:
                 # Close the figure if there are no more images
                 plt.close() 
@@ -97,20 +108,20 @@ class DatasetAnnotation:
             print("Escape key pressed, closing the figure")
             plt.close()
         else:
-            print("Invalid key pressed, please press '1', '2', '3', or '4'")
+            print(self.key_err_msg)
 
     def annotate_images(self):
         """Start annotating the images."""
         # Check if there are images to annotate
-        if self.image_ids_not_annotated:
-            self.image_id = self.get_random_image()  # Get the first image to annotate
+        if self.not_annotated_queue:
+            self.tmp_image_path = self.get_unannotated_image()  # Get the first image to annotate
             fig, ax = plt.subplots()
             self.annotate_ax = ax
             self.annotate_fig = fig
             # Connect the event to the function
             fig.canvas.mpl_connect('key_press_event', self._on_key) 
             # Display the first image
-            self._display_image(self.image_id) 
+            self._display_image(self.tmp_image_path) 
             plt.show()
         else:
             print("No images to annotate.")
