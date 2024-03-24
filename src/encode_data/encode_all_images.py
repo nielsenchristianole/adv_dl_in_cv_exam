ed2044dabce4dfdc0cf80c1d23e8eec72aad5a11
Christian <nielsenchristianole@gmail.com>
2024-03-24 09:47:56 +0100

    updated encode script to fit model refactoring

--- src/encode_data/encode_all_images.py
+++ src/encode_data/encode_all_images.py
@@ -13,20 +13,18 @@ from src.utils.misc import image_path_to_endocing_path
 
 
 @torch.no_grad()
-def main():
+def main(classes: list[str] = ['1', '2', '3', '4']):
     cfg = Config('configs/config.yaml')
     data_folder = cfg.get('data', 'raw_path')
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = CLIPWithHead()#.to(device)
-    encode_func = lambda images: model.encode(images).cpu().numpy()
-    encode_func = lambda images: torch.randn(images.shape[0], 512, device=device).cpu().numpy()
+    model = CLIPWithHead.with_classification_head(classes).to(device).eval()
     
     dataset = AllImageDataset(data_folder, use_relative_path_as_label=True, device=device)
     data_loader = DataLoader(dataset, batch_size=32)
 
     for images, paths in tqdm.tqdm(data_loader):
-        encodings = encode_func(images)
+        encodings = model.embed_image(images).cpu().numpy()
         for rel_path, encoding in zip(paths, encodings):
             encoding_path = image_path_to_endocing_path(rel_path)
             path = os.path.join(data_folder, encoding_path)
