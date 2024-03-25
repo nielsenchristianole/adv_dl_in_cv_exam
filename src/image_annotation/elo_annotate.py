ff69c8c5f9011a05f1b042748252e4b7dbe3ec97
CalleRC <calle@rygecarlsen.dk>
2024-03-25 23:21:17 +0100

    elo annotation

---
+++ src/image_annotation/elo_annotate.py
@@ -0,0 +1,213 @@
+
+
+from pathlib import Path
+from functools import cached_property, cache
+from PIL import Image
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from src.utils.config import Config
+import math
+
+class ELOAnnotate:
+    
+    def __init__(self, data_path : Path, out_path : Path, auto_save_every = 10):
+        
+        self.discard_patters = ['**/never/**']
+        
+        self.data_path = data_path
+        self.out_path = out_path
+        self.out_matches_path = out_path.parent / (out_path.stem + "_matches.txt")
+        self.auto_save_every = auto_save_every
+        
+        if not self.out_path.exists():
+            # Create the output path and file
+            self.out_path.parent.mkdir(parents=True, exist_ok=True)
+            df = pd.DataFrame(columns=['name', 'path', 'elo', 'games', 'discard'])
+            df.to_csv(self.out_path, index=False)
+        if not self.out_matches_path.exists():
+            # Create the output path and file
+            self.out_matches_path.parent.mkdir(parents=True, exist_ok=True)
+            
+        self.total_games = 0
+        self.total_discard = 0
+        
+        fig, axs = plt.subplots(1,2)
+        self.fig = fig
+        self.axs = axs
+        
+        self.annotations : dict[str,tuple[str, float,int,bool]] = self._get_annotations()
+        
+    def _get_annotations(self) -> dict[str,tuple[str, float,int,bool]]:
+        """Return the annotations from the csv file."""
+        df = pd.read_csv(self.out_path)
+        annotations = dict(zip(df['name'], zip(df['path'], df['elo'], df['games'], df['discard'])))
+        
+        for path in self._all_paths:
+            name = path.name
+            if name not in annotations:
+                annotations[name] = (path, 1000, 0, False)
+            else:
+                self.total_games += annotations[name][2]/2
+                self.total_discard += annotations[name][3]
+        
+        return annotations
+
+            
+    def _save_annotations(self):
+        
+        anno_formated = [(name, *values) for name, values in self.annotations.items()]
+        
+        df = pd.DataFrame(anno_formated)
+        df.to_csv(self.out_path, index=False, header=['name', 'path', 'elo', 'games', 'discard'])
+        
+    @cached_property
+    def _all_paths(self) -> set[Path]:
+        """Return all the image paths in the data folder."""
+        _all = set(self.data_path.glob('**/*.jpg'))
+        _all |= set(self.data_path.glob('**/*.png'))
+        for pattern in self.discard_patters:
+            _all -= set(self.data_path.glob(pattern + '/*'))
+        return _all
+    
+    def _pick_images(self):
+        """
+        Pick two images to compare based on their ELO ratings.
+        The probability of picking each image is inversely proportional to the number of games it has been played.
+        """
+
+        # Calculate the probability of picking each image, inversely proportional to the number of games it has been played
+        if self.total_games == 0:
+            probs = {name: 1 for name in self.annotations if not self.annotations[name][3]}
+        else:
+            probs = {name: math.exp(-games) \
+                            for name, (_, _, games, discard)  in self.annotations.items() \
+                            if not discard}
+
+        # Normalize the probs so they sum to 1
+        total_prob = sum(probs.values())
+        probs = {name: prob / total_prob for name, prob in probs.items()}
+
+        # Pick an image based on the probs
+        names, probs = zip(*probs.items())
+        names = np.random.choice(names, 101, p=probs, replace=False)
+        
+        # pick image two as the image with the closest ELO rating to image one
+        name1 = names[0]
+        name2 = min(names[1:], key=lambda name: abs(self.annotations[name][1] - self.annotations[name1][1]))
+        
+        return name1, name2
+    
+
+    def _update_elo(self, name1, name2, result):
+        """
+        Update the rating based on the expected and actual outcomes.
+        
+        result is the result of the match, 1 if the first image won, 0 if the second image won.
+        """
+        # write the names to the matches file
+        with open(self.out_matches_path, 'a') as f:
+            f.write(f"{name1},{name2},{result}\n")
+        
+        path1, rating1, games1, discard1 = self.annotations[name1]
+        path2, rating2, games2, discard2 = self.annotations[name2]
+        
+        expected1 = 1 / (1 + 10 ** ((rating1 - rating2) / 400))
+        expected2 = 1 - expected1
+        
+        rating1 += 32 * (result - expected1)
+        rating2 += 32 * ((1 - result) - expected2)
+        
+        self.total_games += 1
+        
+        if self.total_games % self.auto_save_every == 0:
+            print("Auto saving annotations")
+            self._save_annotations()
+        
+        self.annotations[name1] = (path1, rating1, games1 + 1, discard1)
+        self.annotations[name2] = (path2, rating2, games2 + 1, discard2)
+        
+    def _update_display(self):
+        
+        name1, name2 = self._pick_images()
+        
+        self.tmp_name1 = name1
+        self.tmp_name2 = name2
+
+        path1, _, games1, _ = self.annotations[name1]
+        path2, _, games2, _ = self.annotations[name2]
+        
+        def display(ax, path: Path, games):
+            ax.clear()
+            img = Image.open(path)
+            ax.imshow(img)
+            ax.set_title(path.name + f"\nGames: {games}")
+        
+        for ax, path, games in zip(self.axs, [path1, path2], [games1, games2]):
+            display(ax, Path(path), games)
+        
+        self.fig.texts.clear()
+        self.fig.text(0.7, 0.98, f"'1':pick left, '2':pick right\n \
+                                    't':discard left, 'y':discard right\n \
+                                    'q':quit and save",
+                      verticalalignment='top', horizontalalignment='right', fontsize=10)
+        self.fig.text(0.95, 0.98, f"Games: {self.total_games}\n \
+                                    Discard: {self.total_discard}\n \
+                                    Total: {len(self.annotations)}",
+                      verticalalignment='top', horizontalalignment='right', fontsize=10)
+        
+        self.fig.canvas.draw()
+        
+    def _discard(self, name):
+        path, elo, games, discard = self.annotations[name]
+        self.annotations[name] = (path, elo, games, True)
+        self.total_discard += 1
+    
+    def _on_key(self, event):
+        """Function to be called when a key is pressed."""
+        # Check if the key pressed is a valid annotation
+        
+        match event.key:
+            case '1':
+                self._update_elo(self.tmp_name1, self.tmp_name2, 1)
+            case '2':
+                self._update_elo(self.tmp_name1, self.tmp_name2, 0)
+            case 't':
+                self._discard(self.tmp_name1)
+            case 'y':
+                self._discard(self.tmp_name2)
+            case 'q':
+                print("Escape key pressed, closing and saving")
+                plt.close()
+            case _:
+                print("Invalid key pressed: ", event.key)
+        
+        if event.key in ['1', '2', 't', 'y', 'q']:
+            self._update_display()
+                
+    def annotate(self):
+        
+        self.fig.canvas.mpl_connect('key_press_event', self._on_key) 
+        
+        self._update_display()
+        plt.show()
+        self._save_annotations()
+
+    
+if __name__ == "__main__":
+    cfg = Config('configs/config.yaml')
+    
+    data_folder = Path(str(cfg.get("data", "raw_path")))
+    output_folder = Path(str(cfg.get("data", "elo_annotations_path")))
+
+    # Ask user for name of the output csv file
+    output_csv = input("Please enter your name (This will be the name of the output csv file, use same name as previously to continue labelling): ") + ".csv"
+    output_csv = output_csv.replace(" ", "_").lower()
+
+    print(f"Output csv file will be saved as {output_csv}")
+
+    annotater = ELOAnnotate(data_folder, output_folder / output_csv)
+    annotater.annotate()
+
+
