e6c582c4e7d932129ae8ff99be9b6ee6584a676d
Christian <nielsenchristianole@gmail.com>
2024-03-25 12:39:56 +0100

    inherrit from abstract baseclass

--- src/dataloader/encodings.py
+++ src/dataloader/encodings.py
@@ -9,40 +9,40 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-from src.utils.misc import image_path_to_endocing_path
+from src.utils.config import Config
+from src.utils.misc import image_path_to_encoding_path
+from src.dataloader.base_class import PaintingDataset
 
 
-class EncodedDataset(Dataset):
+def load_whole_dataset(csv_name: Optional[str]) -> torch.Tensor:
+    cfg = Config('configs/config.yaml')
+
+    csv = os.path.join(cfg.get('data', 'annotations_path'), csv_name or 'all_wanted_images.csv')
+    dataset = EncodedDataset(csv, cfg.get('data', 'raw_path'))
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=False)
+    encodings = torch.concat([e for e, l in dataloader])
+    return encodings
+
+
+class EncodedDataset(PaintingDataset):
     def __init__(
         self,
         csv_file: PathLike,
         root_dir: PathLike,
         device: torch.device = torch.device('cpu')
     ) -> None:
-        
-        self.annotations = pd.read_csv(csv_file)
-        self.root_dir = root_dir
+        super().__init__(csv_file, root_dir, device)
 
-        self.label_to_index = {label: i for i, label in enumerate(np.unique(self.annotations['label']))}
-        self.index_to_label = list(self.label_to_index.keys())
-        self.device = device
-
-    def __len__(self) -> int:
-        return len(self.annotations)
-
-    def __getitem__(self, index: int) -> Tuple[Image.Image, torch.Tensor]:
-        _, relative_path, label = self.annotations.iloc[index]
-        relative_path = image_path_to_endocing_path(relative_path)
+    def get_x(self, index: int) -> torch.Tensor:
+        _, relative_path, _, _ = self.annotations.iloc[index]
+        relative_path = image_path_to_encoding_path(relative_path)
         path = os.path.join(self.root_dir, relative_path)
         encoding = np.load(path)
         encoding = torch.from_numpy(encoding).to(self.device)
-        label = torch.tensor(self.label_to_index[label]).to(self.device)
-        return (encoding, label)
+        return encoding
 
 
 if __name__ == "__main__":
-    from src.utils.config import Config
-
     import argparse
     parser = argparse.ArgumentParser(description="Load and display images from a CSV file.")
     parser.add_argument('--file', type=str, help="The name of the CSV file without extension.")
@@ -58,6 +58,7 @@ if __name__ == "__main__":
     csv_path = os.path.join(ann_folder, f"{csv_name}.csv")
 
     dataset = EncodedDataset(csv_file=csv_path, root_dir=data_folder)
+    partial_dataset = dataset.get_dataset_subset('train')
     dataloader = DataLoader(dataset, batch_size=9, shuffle=True)
 
     images, labels = next(iter(dataloader))
