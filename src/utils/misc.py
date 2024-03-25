ff79e96cf61becdf1bee26e20fabf687ae58f2db
Christian <nielsenchristianole@gmail.com>
2024-03-25 12:41:47 +0100

    added hashing fúnktion for creating dataset splits

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,5 +1,6 @@
-import yaml
 import os
+import yaml
+import hashlib
 from os import PathLike
 
 
@@ -15,7 +16,19 @@ def split_path(path: PathLike) -> tuple[str, str, str]:
     return _dir, image_id, ext
 
 
-def image_path_to_endocing_path(image_path: str, encoding_base_path: str = 'endodings') -> str:
+def image_path_to_encoding_path(image_path: str, encoding_base_path: str = 'encodings') -> str:
     with_other_extension = os.path.splitext(image_path)[0] + '.npy'
     encoding_path = os.path.join(encoding_base_path, with_other_extension)
     return encoding_path
+
+
+def hash_image_name(image_name: str) -> float:
+    """
+    Returns a float in the range [0, 1). Used for deterministic data splitting.
+    """
+    precision = 0xffffffff
+    _hash = hashlib.sha256(image_name.encode(), usedforsecurity=False).digest()
+    _int = int.from_bytes(_hash, 'little')
+    normalized = (_int % precision) / precision
+    return normalized
+    
