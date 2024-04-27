a2dfc7a8a4175ece251b1ac5ec60e7ec0a3c33ac
CalleRC <calle@rygecarlsen.dk>
2024-04-27 19:17:34 +0200

    corner detection

--- src/training/corner_detector_predict.py
+++ src/training/corner_detector_predict.py
@@ -45,7 +45,7 @@ def show_predictions(batch, outputs, prefix : str, scale_to=256):
         
 
 # Load the pretrained model
-model = CornerDetector.load_from_checkpoint('model_checkpoints/finalasdjas-v1.ckpt')
+model = CornerDetector.load_from_checkpoint('model_checkpoints/inception.ckpt', map_location='cpu')
 
 # Load the test dataset
 test_dataset = CornerDataset(scale_to=512, is_train=False)
--- src/training/corner_detector_train.py
+++ src/training/corner_detector_train.py
@@ -10,8 +10,9 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 from pytorch_optimizer import create_optimizer
 from pytorch_lightning.utilities.types import OptimizerLRScheduler
 import torch.nn as nn
-import torch.optim as optim
 from dataclasses import dataclass
+import cv2
+import numpy as np
 
 
 @dataclass(frozen=True)
@@ -25,38 +26,43 @@ class TrainConfig:
     weight_decay: float = 1e-6
 
 class CornerDetector(LightningModule):
-    def __init__(self):
+    
+    IMAGENET_MEAN = np.array([0.485, 0.456, 0.406])
+    IMAGENET_STD = np.array([0.229, 0.224, 0.225])
+    
+    def __init__(self, scale_to : int = 512):
         super().__init__()
-        self.inception = inception_v3(pretrained=True, aux_logits=False)
-        self.fc = nn.Linear(2048, 8)  # 8 outputs for corner coordinates
+        self.inception = inception_v3(pretrained=True, aux_logits=True)
+        self.inception.fc = nn.Linear(2048, 8)  # 8 outputs for corner coordinates
         
         self.config = TrainConfig()
+        self.scale_to = scale_to
 
     def forward(self, x):
         x = self.inception(x)
-        x = self.fc(x)
+        # x = self.fc(x)
         return x
 
     def training_step(self, batch, batch_idx):
         images, targets = batch
-        outputs = self(images)
+        outputs = self(images)[0]
         
-        loss = nn.MSELoss()(outputs[:-1], targets[:-1])
+        loss = nn.MSELoss()(outputs, targets)
         
         if batch_idx == 0:
             self.log('train/loss', loss)
-            self._log_predictions(batch, outputs, "train")
+            # self._log_predictions(batch, outputs, "train")
         return loss
 
     def validation_step(self, batch, batch_idx):
         images, targets = batch
         outputs = self(images)
         
-        loss = nn.MSELoss()(outputs[:-1], targets[:-1])
+        loss = nn.MSELoss()(outputs, targets)
         
         if batch_idx <= 10:
             self.log('val/loss', loss)
-            self._log_predictions(batch, outputs, "val")
+            # self._log_predictions(batch, outputs, "val")
             
         return loss
     
@@ -82,12 +88,12 @@ class CornerDetector(LightningModule):
         lr_super = torch.optim.lr_scheduler.OneCycleLR(
             optimizer=optimizer,
             max_lr=self.config.learning_rate_warmup_max,
-            total_steps=self.config.learning_rate_warmup_period,
+            total_steps=self.config.learning_rate_warmup_steps,
         )
         lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
             optimizer=optimizer,
             schedulers=[lr_super, lr_cosine], # type: ignore
-            milestones=[self.config.learning_rate_warmup_period],
+            milestones=[self.config.learning_rate_warmup_steps]
         )
 
         return { # type: ignore
@@ -99,11 +105,46 @@ class CornerDetector(LightningModule):
                 "name": "lr"
             }
         }
+        
+    # def _log_predictions(self, batch, outputs, prefix : str):
+        
+    #     def draw_corners(img, corners, color):
+    #         corners *= self.scale_to
+    #         corners = corners.reshape(4, 2)
+            
+    #         for i in range(len(corners)):
+    #             cv2.line(img, (int(corners[i, 0]), int(corners[i, 1])), (int(corners[(i+1) % 4, 0]), int(corners[(i+1) % 4, 1])), color, 2)
+    #             cv2.putText(img, f"{i}", (int(corners[i, 0]), int(corners[i, 1])), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                
+        
+    #     imgs = batch[0].cpu().numpy()
+    #     corners_target = batch[1].cpu().numpy()
+    #     outputs = outputs.cpu().detach().numpy()
+        
+    #     images = np.zeros_like(imgs)
+    #     for i, (img, pred, target) in enumerate(zip(imgs, outputs, corners_target)):
+    #         img = img.transpose(1, 2, 0)
+            
+    #         img = img * self.IMAGENET_STD + self.IMAGENET_MEAN
+    #         img = np.clip(img, 0, 1)
+    #         img = np.ascontiguousarray(img)
+            
+    #         # draw_corners(img, pred, (0, 1, 0))
+    #         # draw_corners(img, target, (1, 0, 0))
+            
+    #         img = (img * 255).astype(np.uint8)
+    #         images[i] = img.transpose(2, 0, 1)
+            
+    #         if i > 5:
+    #             break
+    #     self.logger.experiment.add_images(f"{prefix}",images.astype(np.uint8), self.current_epoch) # type: ignore
 
 if __name__ == '__main__':
     name = input("Insert name of training:")
     
-    scale_to = 256
+    torch.set_float32_matmul_precision('medium')
+    
+    scale_to = 512
     batch_size = 16
     
     train_dataset = CornerDataset(is_train=True, scale_to=scale_to)
